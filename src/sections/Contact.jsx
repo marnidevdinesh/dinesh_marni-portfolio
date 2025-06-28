114a0cb8bf3c519f4861c5ba37c0aada21a640ec
marnidevdinesh <77916378+marnidevdinesh@users.noreply.github.com>
2025-06-28 17:59:19 +0530

    Update form width

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -38,7 +38,7 @@ let handledata=(e)=>{
   return (
     <div id='contact' className='min-h-screen flex items-center justify-center py-20'>
         <RevelOnScroll>
-            <div className='px-4 w-150'>
+            <div className='px-4 w-80 md:w-150'>
                 <h2 className='text-3xl font-bold mb-8 text-center bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent'>Get In Touch</h2>
              
                     <form action="" className='space-y-6'onSubmit={handlesubmit} >
